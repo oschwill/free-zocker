6c0e53e978dca9877c1fb9c41649e18ec1645358
oschwill <o.schwill@web.de>
2023-11-19 22:18:49 +0100

    Header Design und Navbar Canvas Funktion implementiert

--- src/components/header/HeaderInputBox.jsx
+++ src/components/header/HeaderInputBox.jsx
@@ -1,7 +1,18 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const HeaderInputBox = () => {
   return (
-    <article>
-      <input type="text" />
+    <article className="relative mr-12">
+      <input
+        type="text"
+        className="w-96 h-14 pl-2 rounded-xl bg-inputBackgroundColor text-[2rem]"
+      />
+      <FontAwesomeIcon
+        icon={faMagnifyingGlass}
+        style={{ color: '#ffffff' }}
+        className="h-7 absolute right-5 top-3"
+      />
     </article>
   );
 };
