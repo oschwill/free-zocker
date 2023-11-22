da9a43b7e335397c0095d83b6acbbfb8f6d4536b
oschwill <o.schwill@web.de>
2023-11-22 15:55:52 +0100

    Top 4 PC Games Card

--- src/components/main/home/ItemCard.jsx
+++ src/components/main/home/ItemCard.jsx
@@ -1,20 +1,43 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const ItemCard = ({ item }) => {
+const ItemCard = ({ item, hasDescription, verticalDirection, index }) => {
   return (
-    <article className="flex flex-col text-white w-[20vw] overflow-hidden bg-boxBackgroundColor rounded-3xl">
-      <img src={item.thumbnail} alt="thumbnail" className="rounded-3xl" />
-      <div className="flex flex-col mt-12 ml-[1vw] mr-[1vw] justify-center gap-16 ">
+    <article
+      className={`flex text-white overflow-hidden relative bg-boxBackgroundColor rounded-3xl ${
+        verticalDirection ? 'flex-col  w-[20vw]' : 'items-center mr-[10%] w-[90%] h-[95%]'
+      }`}
+    >
+      <img
+        src={item.thumbnail}
+        alt="thumbnail"
+        className={`rounded-3xl ${verticalDirection ? '' : 'w-[50%]'}`}
+      />
+      {index && (
+        <div className="absolute top-10 left-10 flex items-center justify-center border-2 rounded-full w-20 h-20 border-buttonBackgroundColor bg-boxBackgroundColor text-center">
+          <p className="text-2.5rem  text-white">{index + 1}</p>
+        </div>
+      )}
+      <div
+        className={`flex flex-col  mt-12 ml-[1vw] mr-[1vw] justify-center ${
+          verticalDirection ? 'gap-16' : 'gap-6'
+        }`}
+      >
         <h2 className="text-white text-2.5rem h-[2vh] ">{item.title} </h2>
-        <p className="text-2rem opacity-75 h-[7.5vh] ">{item.short_description}</p>
+        <p className={`text-2rem opacity-75 ${hasDescription ? 'h-[7.5vh]' : ''}`}>
+          {hasDescription ? item.short_description : ''}
+        </p>
         <Link
           to={`/details/${item.id}`}
-          className="p-3 bg-buttonBackgroundColor rounded-2xl text-center text-2.5rem hover:opacity-75 lg:w-[100%] xl:w-[50%]"
+          className="p-3 bg-buttonBackgroundColor rounded-2xl text-center text-2.5rem hover:opacity-75 w-[175px]"
         >
           READ MORE
         </Link>
-        <div className="flex items-center gap-6 border-t-2 border-borderColorWhite pt-12 pb-12 ">
+        <div
+          className={`flex items-center gap-6 pt-12 pb-12 ${
+            verticalDirection ? 'border-t-2 border-borderColorWhite' : ''
+          }`}
+        >
           <div className="border-2 p-4 rounded-3xl border-buttonBackgroundColor">
             <img
               className="w-8 h-8 "
@@ -26,7 +49,7 @@ const ItemCard = ({ item }) => {
               alt="platform"
             />
           </div>
-          <p className="text-2.5rem border-2 pl-4 pr-4 rounded-3xl border-buttonBackgroundColor">
+          <p className="text-2.5rem border-2 pl-4 pr-4 rounded-3xl border-buttonBackgroundColor text-white">
             {item.genre}
           </p>
         </div>
@@ -37,6 +60,9 @@ const ItemCard = ({ item }) => {
 
 ItemCard.propTypes = {
   item: PropTypes.object,
+  hasDescription: PropTypes.bool,
+  verticalDirection: PropTypes.bool,
+  index: PropTypes.number,
 };
 
 export default ItemCard;
